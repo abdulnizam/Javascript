e71e99f87079e414647136724fe5a498d5fa51a0
Abdulnizam <abdulnizam@freniz.com>
2018-10-01 22:56:20 +0400

    updated the new changes on blockchain folder

---
+++ Python/Blockchain/Blockchain.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+from Block import Block
+
+class Blockchain:
+
+    diff = 20
+    maxNonce = 2**32
+    target = 2 ** (256-diff)
+
+    block = Block("Genesis")
+    dummy = head = block
+
+    def add(self, block):
+
+        block.previous_hash = self.block.hash()
+        block.blockNo = self.block.blockNo + 1
+
+        self.block.next = block
+        self.block = self.block.next
+
+    def mine(self, block):
+        for n in range(self.maxNonce):
+            if int(block.hash(), 16) <= self.target:
+                self.add(block)
+                print(block)
+                break
+            else:
+                block.nonce += 1
--- Python/Blockchain/README.md
+++ Python/Blockchain/README.md
@@ -3,3 +3,6 @@
 Here the example to know how the blockchain works
 
 try on python3 
+
+1) Dependency Module example 
+2) Blockchain concepts
